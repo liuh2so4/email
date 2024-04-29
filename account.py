4e31c010ae6093e830e8aab5fd9178c12d01be46
liuh2so4 <71363099+liuh2so4@users.noreply.github.com>
2024-04-30 03:54:18 +0800

    Create account.py

---
+++ account.py
@@ -0,0 +1,20 @@
+def getacc():
+  return "your gmail"
+
+def getpw():
+    return "your password"
+
+def useracc():
+    return "your user gmail"
+
+def userpw():
+    return "your user password"
+
+def mysqlacc():
+    return "your db account"
+
+def mysqlpw():
+    return "your db password"
+
+def mysqldb():
+    return "your db name"
